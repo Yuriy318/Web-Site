2b275d589daa67fa428aca663fb238c3b6b76dcd
Yuriy318 <77587475+Yuriy318@users.noreply.github.com>
2021-01-18 00:55:28 +0500

    Добавление index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta content="">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+     <link rel="stylesheet" type="text/css" href="main.css">
+    <title>Список лабораторных работ Ефимов Ю. А.</title>
+  </head>
+  <body>
+    <ul id="nav">
+      <li><a href="WWW/lab_1/" class="link">Лабораторная работа 1</a>
+      <li><a href="WWW/lab_2/" class="link">Лабораторная работа 2</a>
+      <li><a href="WWW/lab_3/" class="link">Лабораторная работа 3</a>
+      <li><a href="WWW/lab_4/planets/index2.php" class="link">Лабораторная работа 4</a>
+      <li><a href="WWW/lab_5/" class="link">Лабораторная работа 5</a>
+    </ul>
+
+  </body>
+</html>
